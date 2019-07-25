a4694cca3e61a7fe4d1d5ca2e1f1cf06598842ff
wellhz1n <wellingtom1234520@gmail.com>
2019-07-25 19:33:07 -0300

    ADAPTATION FOR PC

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -25,32 +25,36 @@ public class Player : MonoBehaviour
     {
         if (!Global.paused)
         {
-
-
-            if (Input.touchCount > 0 && Input.touchCount < 2)
+            if (SystemInfo.deviceType == DeviceType.Handheld)
             {
-                var resolution = Screen.width;
-                var p = Input.GetTouch(0).position.x;
-
-                if (p >= resolution - resolution && p <= resolution / 2 || (Input.GetAxis("Horizontal") < 0f))
+                if (Input.touchCount > 0 && Input.touchCount < 2)
                 {
-                    transform.RotateAround(Vector3.zero, Vector3.forward, vel * Time.fixedDeltaTime * +vel);
-
-                }
-                else if (p >= (resolution / 2) + 1 && p <= resolution || (Input.GetAxis("Horizontal") > 0f))
-                {
-                    transform.RotateAround(Vector3.zero, Vector3.forward, vel * Time.fixedDeltaTime * -vel);
-
+                    var resolution = Screen.width;
+                    var p = Input.GetTouch(0).position.x;
+                    if (p >= resolution - resolution && p <= resolution / 2)
+                        transform.RotateAround(Vector3.zero, Vector3.forward, vel * Time.fixedDeltaTime * +vel);
+                    else if (p >= (resolution / 2) + 1 && p <= resolution)
+                        transform.RotateAround(Vector3.zero, Vector3.forward, vel * Time.fixedDeltaTime * -vel);
                 }
             }
+            if (SystemInfo.deviceType == DeviceType.Desktop)
+            {
+                if (Input.GetAxis("Horizontal") < 0f)
+                    transform.RotateAround(Vector3.zero, Vector3.forward, vel * Time.fixedDeltaTime * +vel);
+                else if (Input.GetAxis("Horizontal") > 0f)
+                    transform.RotateAround(Vector3.zero, Vector3.forward, vel * Time.fixedDeltaTime * -vel);
+            }
+
+
         }
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if (collision.name == "hex(Clone)")
         {
-            
+
 
             Global.Mostrar(Menu.gameover);
             Global.Stop(Menu.gameover);
